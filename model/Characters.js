ca4f559531691b916ac53e487ed3977101678ca7
Terence Tat <terence.wy.tat@gmail.com>
2021-04-18 21:58:07 -0600

    commit

---
+++ model/Characters.js
@@ -0,0 +1,56 @@
+const knight = {
+    name: 'Knight',
+    vigor: 12,
+    strength: 13,
+    vitality: 15,
+    speed: 10,
+    equipment: [longSword.name,knightShield.name,knightHelmet.name,knightArmor.name,knightGuantlets.name,knightLeggings.name]
+};
+
+const longSword = {
+    name: 'Long Sword',
+    vigor: 0,
+    strength: 11,
+    vitality: 0,
+    speed: 0,
+}
+
+const knightShield = {
+    name: 'Knight Shield',
+    vigor: 0,
+    strength: 0,
+    vitality: 10,
+    speed: 0,
+}
+
+const knightHelmet = {
+    name: 'Knight Helmet',
+    vigor: 10,
+    strength: 0,
+    vitality: 0,
+    speed: 0,
+}
+
+const knightArmor = {
+    name: 'Knight Armor',
+    vigor: 10,
+    strength: 11,
+    vitality: 0,
+    speed: 0,
+}
+
+const knightGuantlets = {
+    name: 'Knight Guantlets',
+    vigor: 5,
+    strength: 11,
+    vitality: 0,
+    speed: 0,
+}
+
+const knightLeggings = {
+    name: 'Knight Leggings',
+    vigor: 5,
+    strength: 11,
+    vitality: 0,
+    speed: 0,
+}
