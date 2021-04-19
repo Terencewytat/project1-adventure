ffc8a1247d3cb1b0bc4db9f233829cd74cb1caab
Terence Tat <terence.wy.tat@gmail.com>
2021-04-18 22:30:36 -0600

    commit

--- model/Characters.js
+++ model/Characters.js
@@ -1,9 +1,25 @@
+const character = {
+    knight,
+    warrior
+}
+
+console.log(character.knight.equipment[0])
+
 const knight = {
     name: 'Knight',
     vigor: 12,
     strength: 13,
     vitality: 15,
-    speed: 10,
+    speed: 12,
+    equipment: [longSword.name,knightShield.name,knightHelmet.name,knightArmor.name,knightGuantlets.name,knightLeggings.name]
+};
+
+const warrior = {
+    name: 'Warrior',
+    vigor: 13,
+    strength: 16,
+    vitality: 10,
+    speed: 13,
     equipment: [longSword.name,knightShield.name,knightHelmet.name,knightArmor.name,knightGuantlets.name,knightLeggings.name]
 };
 
@@ -11,6 +27,14 @@ const longSword = {
     name: 'Long Sword',
     vigor: 0,
     strength: 11,
+    vitality: 0,
+    speed: 0,
+}
+
+const battleAxe = {
+    name: 'Battle Axe',
+    vigor: 0,
+    strength: 14,
     vitality: 0,
     speed: 0,
 }
@@ -23,6 +47,14 @@ const knightShield = {
     speed: 0,
 }
 
+const roundShield = {
+    name: 'Round Shield',
+    vigor: 0,
+    strength: 0,
+    vitality: 8,
+    speed: 0,
+}
+
 const knightHelmet = {
     name: 'Knight Helmet',
     vigor: 10,
@@ -31,10 +63,26 @@ const knightHelmet = {
     speed: 0,
 }
 
+const northernHelmet = {
+    name: 'Northern Helmet',
+    vigor: 5,
+    strength: 0,
+    vitality: 0,
+    speed: 0,
+}
+
 const knightArmor = {
     name: 'Knight Armor',
     vigor: 10,
-    strength: 11,
+    strength: 0,
+    vitality: 0,
+    speed: 0,
+}
+
+const northernArmor = {
+    name: 'Northern Armor',
+    vigor: 10,
+    strength: 0,
     vitality: 0,
     speed: 0,
 }
@@ -42,7 +90,15 @@ const knightArmor = {
 const knightGuantlets = {
     name: 'Knight Guantlets',
     vigor: 5,
-    strength: 11,
+    strength: 0,
+    vitality: 0,
+    speed: 0,
+}
+
+const northernGuantlets = {
+    name: 'Northern Guantlets',
+    vigor: 5,
+    strength: 0,
     vitality: 0,
     speed: 0,
 }
@@ -50,7 +106,15 @@ const knightGuantlets = {
 const knightLeggings = {
     name: 'Knight Leggings',
     vigor: 5,
-    strength: 11,
+    strength: 0,
+    vitality: 0,
+    speed: 0,
+}
+
+const northernLeggings = {
+    name: 'Northern Leggings',
+    vigor: 5,
+    strength: 0,
     vitality: 0,
     speed: 0,
 }
---
+++ model/combat.js
@@ -0,0 +1,7 @@
+// let effectiveVigor = (
+//
+// )
+
+function calcEffectiveStats() {
+
+}
