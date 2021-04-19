2001c8e1e689a36741a2593347dcb877d8cbc7d4
Terence Tat <terence.wy.tat@gmail.com>
2021-04-18 21:26:09 -0600

    commit

--- model/adventure.js
+++ model/adventure.js
@@ -1,17 +1,16 @@
 const scenes = require('./scenes')
 
-scenes.createScene("Intro","Yes indeed. It is called Lothric. Where the transitory lands of the Lords of Cinder converge. In venturing north, the pilgrims discover the truth of the old words. The fire fades. And the lords go without thrones. When the link of fire is threatened, the bell tolls; Unearthing the old lords of cinder from their graves. Aldrich Saint of the Deep; Farron’s Undead Legion the Abyss Watchers; and their reclusive lord of the profaned capitol Yhorm the Giant. Only in truth, the lords will abandon their thrones. And the unkindled will rise. Nameless accursed undead, unfit even to be cinder. And so it is, that ash seeketh embers.",
+scenes.createScene("Intro","\n Yes indeed. It is called Lothric. Where the transitory lands of the Lords of Cinder converge. In venturing north, the pilgrims discover the truth of the old words. The fire fades. And the lords go without thrones. When the link of fire is threatened, the bell tolls; Unearthing the old lords of cinder from their graves. Aldrich Saint of the Deep; Farron’s Undead Legion the Abyss Watchers; and their reclusive lord of the profaned capitol Yhorm the Giant. Only in truth, the lords will abandon their thrones. And the unkindled will rise. Nameless accursed undead, unfit even to be cinder. And so it is, that ash seeketh embers.",
 [ 
-    { description: "Choice option one", scene: "Act_1_The_Beginning" }
+    { description: "Begin you're journey Ashen One.", scene: "Cemetary_of_Ash" }
 ])
 
-scenes.createScene("Act_1_The_Beginning","Body of Text in the scenario for Act 1",
+scenes.createScene("Cemetary_of_Ash","Body of Text in the scenario for Act 1",
 [ 
-    { description: "Choice option one", scene: "Scene_name" },
-    { description: "Choice option two", scene: "Scene_name" }
+    { description: "Proceed.", scene: "First_Encounter" },
 ])
 
-scenes.createScene("Act 2: Winter is Coming","Body of Text in the scenario",
+scenes.createScene("First_Encounter","Body of Text in the scenario",
 [ 
     { description: "Choice option one", scene: "Scene_name" },
     { description: "Choice option two", scene: "Scene_name" }
