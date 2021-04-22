afdbdd08b63b5659c789093628b592e521c96bb4
Terence Tat <terence.wy.tat@gmail.com>
2021-04-22 17:38:23 -0600

    commit

--- model/darksouls3.js
+++ model/darksouls3.js
@@ -16,13 +16,52 @@ scenes.createScene("First_Encounter","\n You defeat the hooded man! His body lay
     { description: "Go Right.", scene: "Submerged_Pathway" }
 ])
 
-scenes.createScene("Stone_Pathway","Body of Text in the scenario",
+scenes.createScene("Submerged_Pathway","\n You find yourself walking down the submerged pathway. The water reaches just above your knees as you navigate your feet through the mud, rock, and grass. The further you go, the light illuminating the path gets darker and darker. The path leads into a trench. As two cliff faces block sunlight from entering. The deeper you go the way forward begins to fade and your other senses start to enhance. For what you cannot see, you can hear and smell. Your footsteps sloshing through the water and the smell of rotting corpses in the air. Shortly thereafter, you notice the water begin to shallow and the path begin to widen. Here you see a faint soul sitting on top of a rotten corpse at the end of the path. Surrounded by the walls of the trench, you notice that this poor fellow must've fallen in from great heights and died here.",
+[ 
+    { description: "Touch the faint light.", scene: "Soul_of_a_deserted_corpse" },
+    { description: "Go back.", scene: "COA_Crossroads" }
+])
+
+scenes.createScene("Soul_of_a_deserted_corpse","You touch the light and pick up the Soul of a Deserted Corpse. It does not weigh anything and you slide it into your bag.",
+[ 
+    { description: "Go back.", scene: "COA_Crossroads" },
+])
+
+scenes.createScene("COA_Crossroads","You are now in the middle of the crossroad. You look left and see what looks like a stone path towards the exit of cemetary. You look right and see a narrow pathway submerged in shallow water leading towards a cavern.",
+[ 
+    { description: "Go left.", scene: "Stone_Pathway" },
+])
+
+scenes.createScene("Stone_Pathway","You make your way on to the stone path. The stone tiles look worn and aged. The weather really has done a number on this place. Tiles are upheaved and tree roots have pushed the slabs of stone around. The path is hard to walk on, but you can see that it leads towards the middle of the cemetary. There in the middle, you see what looks like a broken stone fountain with a couple deceased corpses laying about. One corpose on the ground covered in a black cloak. One corpose propped up against the broken fountain with an item clasped in its hand. As you make your way towards the fountain, the corpse cloaked in black suddenly springs up and charges at you with a a dagger!",
+[ 
+    { description: "Proceed to fight!", scene: "Second_Encounter" },
+    { description: "Run!", scene: "Escape_Second_Encounter" }
+])
+
+scenes.createScene("Second_Encounter","You defeat the cloaked man! That was quite the shock. But nonetheless, you carry on forward. Making your way to the fountain you see an item clenched in the fist of the corpse. It looks like a bottle.",
+[ 
+    { description: "Pick it up.", scene: "Ashen_Estus_Flask" },
+    { description: "Keep on walking down the stone path.", scene: "Middle_of_the_Cemetary" }
+])
+
+scenes.createScene("Ashen_Estus_Flask","You bend down to the corpse and notice that it is holding onto a flask. You pry his fingers open and free the item. You just aquired flask of Ashen Estus.",
+[ 
+    { description: "Keep on walking down the stone path.", scene: "Middle_of_the_Cemetary" }
+])
+
+scenes.createScene("Epilogue","Body of Text in the scenario",
 [ 
     { description: "Choice option one", scene: "Scene_name" },
     { description: "Choice option two", scene: "Scene_name" }
 ])
 
-scenes.createScene("Submerged_Pathway","Body of Text in the scenario",
+scenes.createScene("Epilogue","Body of Text in the scenario",
+[ 
+    { description: "Choice option one", scene: "Scene_name" },
+    { description: "Choice option two", scene: "Scene_name" }
+])
+
+scenes.createScene("Epilogue","Body of Text in the scenario",
 [ 
     { description: "Choice option one", scene: "Scene_name" },
     { description: "Choice option two", scene: "Scene_name" }
